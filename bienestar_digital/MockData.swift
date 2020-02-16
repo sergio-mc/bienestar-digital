a0ec1050e6a9ca7f724cb06d8f10ba495c3cd614
Sergio Marrón Calle <sergiodev0106@gmail.com>
2020-02-16 01:44:08 +0100

    Google SDK removed , MockData to login & UsageSpots shown with MapKit

---
+++ bienestar_digital/MockData.swift
@@ -0,0 +1,9 @@
+//
+//  MockData.swift
+//  bienestar_digital
+//
+//  Created by Sergio on 15/02/2020.
+//  Copyright © 2020 sergio-mc. All rights reserved.
+//
+
+import Foundation
---
+++ bienestar_digital/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  bienestar_digital
+//
+//  Created by Sergio on 15/02/2020.
+//  Copyright © 2020 sergio-mc. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
